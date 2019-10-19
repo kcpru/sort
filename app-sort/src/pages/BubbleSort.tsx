d773ff686fc35e0ba7f91073102c569b530b610f
Kacper Ślusarczyk <kacper12345678909@gmail.com>
2019-10-19 18:53:20 +0200

    Added react app-sort

---
+++ app-sort/src/pages/BubbleSort.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+export default function BubbleSort() {
+  return <main>BubbleSort</main>;
+}
---
+++ app-sort/src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+export default function Home() {
+  return <main>Hello</main>;
+}
